7bb42bd673cb3c8ceb882a08be8d94097e8c54f3
松本 和馬 <hello@mtmtkzm.com>
2016-05-02 14:14:26 +0900

    AutoPrefixでプレフィックス自動付与タスクを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
 
 /* Server
 ****************************** */
@@ -28,9 +29,14 @@ gulp.task('sass', function() {
 		.pipe(plumber())
 		.pipe(sass())
 		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
 		.pipe(gulp.dest('./app/public/css'))
 		.pipe(browser.reload({stream:true}))
 });
+
 
 /* Combine MediaQuery
 ****************************** */
